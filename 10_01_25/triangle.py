fba76747abe907e9def2b42d9c52d5fe3ab1b25b
Stella <63104132+StellarSeal@users.noreply.github.com>
2025-01-14 09:45:06 +0700

    Update triangle.py

--- 10_01_25/triangle.py
+++ 10_01_25/triangle.py
@@ -2,6 +2,6 @@ a = float(input("Nhap vao canh A: "))
 b = float(input("Nhap vao canh B: "))
 c = float(input("Nhap vao canh C: "))
 
-is_triangle = (a + b >= c) and (a + c >= b) and (b + c >= a)
+is_triangle = (a + b > c) and (a + c > b) and (b + c > a)
 
 print("3 canh nhap vao la cua tam giac:", is_triangle)
