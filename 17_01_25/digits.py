ac266d955290b46d1ae2320d1808d10687098bab
Stella <63104132+StellarSeal@users.noreply.github.com>
2025-01-17 11:29:41 +0700

    Create digits.py

---
+++ 17_01_25/digits.py
@@ -0,0 +1,27 @@
+n = int(input("Nhap vao so nguyen n: "))
+d = int(input("Nhap vao so nguyen d: "))
+
+nlen = 0
+ndupe = n
+while ndupe > 0:
+    nlen += 1
+    ndupe //= 10
+nred = n
+ndupe = n
+cflag = 0
+for i in range(1, (nlen + 1)):
+    ndupe = nred
+    ndupe //= 10
+    print(ndupe)
+    if ndupe == d:
+        cflag += 1
+        break
+    else:
+        ndupe = nred
+    print(ndupe)
+    ndupe %= (10 ** (nlen - i))
+    if ndupe == d:
+        cflag += 1
+        break
+    nred //= 10
+print(cflag > 0)
