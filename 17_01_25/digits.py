80d58b5b03ee2fb9e4b870c8cca5f623eb629117
Stella <63104132+StellarSeal@users.noreply.github.com>
2025-01-21 09:17:12 +0700

    Update digits.py

--- 17_01_25/digits.py
+++ 17_01_25/digits.py
@@ -2,24 +2,26 @@ n = int(input("Nhap vao so nguyen n: "))
 d = int(input("Nhap vao so nguyen d: "))
 
 nlen = 0
+dlen = 0
 ndupe = n
+ddupe = d
 while ndupe > 0:
     nlen += 1
     ndupe //= 10
-nred = n
-ndupe = n
-cflag = 0
-for i in range(1, (nlen + 1)):
-    ndupe = nred
-    ndupe //= 10
-    if ndupe == d:
-        cflag += 1
+while ddupe > 0:
+    dlen += 1
+    ddupe //= 10
+
+flag = 0
+
+for i in range(1, (nlen - dlen) + 1):
+    ndupe = n % (10 ** (nlen - i))
+    for k in range(1, (nlen - dlen - i) + 1):
+        ndupe //= 10
+        if ndupe == d:
+            flag += 1
+            break
+    if flag != 0:
         break
-    else:
-        ndupe = nred
-    ndupe %= (10 ** (nlen - i))
-    if ndupe == d:
-        cflag += 1
-        break
-    nred //= 10
-print(cflag > 0)
+
+print(flag != 0)
